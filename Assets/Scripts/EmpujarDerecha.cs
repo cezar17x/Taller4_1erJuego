a573d8d27b38d873187d972d9bf23ac8cfc63652
cezar17x <cesar.marquesr@alumnos.arcos.cl>
2024-09-08 02:13:32 -0300

    obstaculos
    pendiente por hacer: Menu, Pausa con restart, GDD y mas niveles

---
+++ Assets/Scripts/EmpujarDerecha.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmpujarDerecha : MonoBehaviour
+{
+    public float fuerza = 5f;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("BallGolf"))
+        {
+            Vector3 impulso = transform.right;
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(impulso * fuerza, ForceMode.Impulse);
+        }
+    }
+}
